1531a7185bbe1ea54c12c9ebb1ac29c9abba90ff
shli3 <shlimoonsteven@gmail.com>
2019-08-21 19:07:06 +0100

    First scene + first commit

---
+++ Assets/Scripts/BlinkingText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+using TMPro;
+
+public class BlinkingText : MonoBehaviour {
+
+	public TextMeshProUGUI words;
+	void Start () {
+
+		words = GetComponent<TextMeshProUGUI>();
+		StartBlinking();
+	}
+	
+	IEnumerator Blink(){
+		
+		while(true){
+			switch(words.color.a.ToString()){
+				case "0":
+					words.color = new Color(words.color.r, words.color.g, words.color.b, 1);
+					yield return new WaitForSeconds(0.75f);
+					break;
+				case "1":
+					words.color = new Color(words.color.r, words.color.g, words.color.b, 0);
+					yield return new WaitForSeconds(0.75f);
+					break;
+			}
+		}
+	}
+
+	void StartBlinking(){
+		StopCoroutine("Blink");
+		StartCoroutine("Blink");
+	}
+}
