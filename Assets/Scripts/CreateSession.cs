82c61aecf737b5bc922b938df77e901fc05fe66a
shli3 <shlimoonsteven@gmail.com>
2020-04-01 21:19:55 +0100

    Reconstructed structure and added a temp.txt for ease of access to past format

--- Assets/Scripts/CreateSession.cs
+++ Assets/Scripts/CreateSession.cs
@@ -9,46 +9,25 @@ Allows User to create session.
 Stores contents in scrollview into file to be accessed in LoadSession
 Must also display user input in scrollview
 */
-public class CreateSession : MonoBehaviour {
+public class CreateSession : UNDelegate {
 
-	public int counter;
-	string[] delegates = new string[200];
+	int counter;
+	UNDelegate[] delegates = new UNDelegate[200];
 	public InputField PotentialD;
 	public GameObject prefab;
 
 	public void InsertDelegate(){
-
-		delegates[counter] = PotentialD.text;
-		Debug.Log(" " + delegates[counter] + " " + counter);
+		delegates[counter] = new UNDelegate(PotentialD.text);
 		counter++;
-		
+		DisplayDelegate();
 	}
 
 	public void DisplayDelegate(){
-
-		GameObject newObj;
+		GameObject newObj = new GameObject();
 
 		for(int i = 0; i < counter; i++){
-
-
-			newObj = (GameObject)Instantiate(prefab, transform);
-
-			newObj.GetComponent<Text>().text = delegates[i];
-
-			
+			Debug.Log("" + delegates[i].country + "");
+			newObj.GetComponent<Text>().text = delegates[i].country;
 		}
-
 	}
-
-	public void NextScene(){
-
-		SceneManager.LoadScene("Session_Name");
-
-	}
-
-	public void CreateSessionFile(){
-
-
-	}
-
 }
---
+++ Assets/Scripts/UNDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UNDelegate : MonoBehaviour
+{
+    
+    public string country;
+    public string attendance { get; private set;}  //A -> Absent, P -> Present (Can abstain), PV -> Present & Voting (Must vote)
+
+    
+    public UNDelegate(){
+        country = "Neverland";
+        attendance = "A";
+    }
+
+    public UNDelegate(string land){
+        country = land;
+        attendance = "A";
+    }
+
+    public UNDelegate(string land, string RollCall){
+        country = land;
+        attendance = RollCall;
+    }
+
+
+}
